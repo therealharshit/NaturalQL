7827d21df45ccd368471083b2f6f854b6492247c
Harshit Verma <therealharshit014@gmail.com>
2026-05-20 21:18:46 +0530

    fix: make production build deterministic

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["pgsql-parser", "libpg-query"],
 };
 
 export default nextConfig;
